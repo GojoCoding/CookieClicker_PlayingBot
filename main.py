a733361bbe089a1a29c85611386b17fc57a922e2
Miguel Beltran <m.beltrann676@gmail.com>
2023-06-14 12:25:31 -0700

    SeleniumWebDriver SetUp, Connected to CookieClicker, Setup for automated clicking of cookie and first upgrade

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-# Testing Repo
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+# ChromeDriver is what connects Selenium to Chrome
+chromeDriverPath = "/Users/mig/Development/chromedriver_mac_arm64/chromedriver"
+chrome_options = Options()
+# Keeps browser open
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+duration = 10
+end_time = time.time() + duration
+while time.time() < end_time:
+    pass
+
+cookiesAccept = driver.find_element(By.XPATH, '/html/body/div[1]/div/a[1]')
+cookiesAccept.click()
+
+languageSelect = driver.find_element(By.ID, "langSelect-EN")
+languageSelect.click()
+
+# Clicking Cookie Button
+cookieButton = driver.find_element(By.ID, "bigCookie")
+while True:
+    cookieButton.click()
+    try:
+        cursor = driver.find_element(By.CSS_SELECTOR, "#sectionRight #products .unlocked.enabled")
+        cursor.click()
+    except NoSuchElementException:
+        time.sleep(1)
+    time.sleep(1)
